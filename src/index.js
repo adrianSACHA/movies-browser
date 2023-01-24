106a42199800636880a1371d6783b2f7e5542178
Radek Bubienko <radek@bubienko.pl>
2023-01-24 20:23:25 +0100

    deleted unnesessary thinks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './core/App';
-import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './GlobalStyle';
-import { theme } from './theme'
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./core/App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
