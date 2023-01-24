ab1384f5d13889d6243ba7b5f81f3e8b6287e2f1
Konrad Kapica <konradkapica@gmail.com>
2023-01-24 23:11:58 +0100

    component MoviePage created

---
+++ src/features/MoviesBrowser/MoviePage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "./Container";
+import MovieTile from "./MovieTile"
+
+const MoviePage = () => (
+    <Container>
+        <MovieTile />
+    </Container>
+);
+
+export default MoviePage;
