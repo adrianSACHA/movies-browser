123844aca91fcb65f81bc49e0923a8d14a8e4e6c
Konrad Kapica <konradkapica@gmail.com>
2023-01-25 13:21:21 +0100

    Description and Rating added to MovieTile

--- src/features/MoviesBrowser/MoviePage/MovieTile/index.js
+++ src/features/MoviesBrowser/MoviePage/MovieTile/index.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { Container, Image, Content, Title, Year, Tags, Tag } from "./styled";
+import { Container, Image, Content, Title, SubTitle, Tags, Tag, Description, Rating, RateBig, RateSmall, Votes } from "./styled";
 import poster from "./images/poster.png"
+import { ReactComponent as Star } from "./icons/Star.svg"
 
 const MovieTile = () => (
     <Container>
-        <Image src={poster} alt=""/>
+        <Image src={poster} alt="" />
         <Content>
             <Title>Mulan</Title>
-            <Year>2020</Year>
+            <SubTitle>2020</SubTitle>
             <Tags>
                 <Tag>Action</Tag>
                 <Tag>Adventure</Tag>
                 <Tag>Drama</Tag>
             </Tags>
+            <Rating>
+                <Star />
+                <RateBig>7,8</RateBig>
+                <RateSmall>/ 10</RateSmall>
+                <Votes>335 votes</Votes>
+            </Rating>
+            <Description>
+                A young Chinese maiden disguises herself as a male warrior in order to save her father.
+                Disguises herself as a male warrior in order to save her father.  A young Chinese maiden disguises herself as a male warrior in order to save her father.
+            </Description>
         </Content>
     </Container>
 );
--- src/features/MoviesBrowser/MoviePage/MovieTile/styled.js
+++ src/features/MoviesBrowser/MoviePage/MovieTile/styled.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
     margin: auto;
     padding: 40px;
     margin-top: 64px;
-    background: ${({theme}) => theme.colors.white};
+    background: ${({ theme }) => theme.colors.white};
     box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
     display: grid;
     grid-template-columns: auto 1fr;
@@ -24,15 +24,15 @@ export const Title = styled.header`
     font-weight: 600;
     font-size: 36px;
     line-height: 1.2;
-    color: ${({theme}) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.black};
     margin-bottom: 24px;
 `;
 
-export const Year = styled.div`
+export const SubTitle = styled.div`
     font-weight: 400;
     font-size: 22px;
     line-height: 1.2;
-    color: ${({theme}) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.black};
 `;
 
 export const Tags = styled.ul`
@@ -44,9 +44,44 @@ export const Tags = styled.ul`
 `;
 
 export const Tag = styled.li`
-    background: ${({theme}) => theme.colors.mystic};
+    background: ${({ theme }) => theme.colors.mystic};
     padding: 8px 16px;
     font-size: 14px;
     margin: 8px;
     border-radius: 5px;
+`;
+
+export const Rating = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const RateBig = styled.span`
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 1.3;
+    margin:0 8px;
+`;
+
+export const RateSmall = styled.span`
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1.2;
+    margin-right: 12px;
+    padding-bottom: 3px;
+    align-self: flex-end;
+`;
+
+export const Votes = styled.span`
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 120%;
+    padding-bottom: 3px;
+    align-self: flex-end;
+`;
+
+export const Description = styled.p`
+    font-size: 20px;
+    line-height: 1.6;
+    margin: 24px 0 0 0;
 `;
