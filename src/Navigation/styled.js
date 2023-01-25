c4f596de59ca3f1f6885622555f70f185a3abda0
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-25 18:45:04 +0100

    added Navigation to updated Main branch

---
+++ src/Navigation/styled.js
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+import { ReactComponent as Video } from "./Video.svg";
+
+export const Wrapper = styled.div`
+  max-width: 1368px;
+  background: #18181b;
+  min-height: 94px;
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+flex-wrap: wrap;
+  align-items: center;
+
+  @media (max-width: 420px) {
+    height: 142px;
+
+  }
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 17px;
+
+  @media (max-width: 420px) {
+    gap: 10px;
+  }
+`;
+
+export const Icon = styled(Video)`
+  height: 40px;
+  width: 40px;
+  border-radius: 0px;
+
+  @media (max-width: 420px) {
+    height: 10px;
+    width: 13px;
+  }
+`;
+
+export const Text = styled.p`
+  width: 168px;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 40px;
+  letter-spacing: -1.5px;
+  text-transform: capitalize;
+  text-align: left;
+
+
+  @media (max-width: 420px) {
+    font-weight: 500;
+    font-size: 13px;
+    width: 95px;
+  }
+`;
+
+export const Menu = styled.div`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 21px;
+  text-transform: uppercase;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MenuItem = styled.span`
+  padding: 8px 24px;
+  /* width: 98px;
+min-height: 48px; */
+  /* border: 1px solid white; */
+  border-radius: 24px;
+
+  &:hover {
+    border: 1px solid white;
+  }
+
+  &:active {
+    border: 1px solid white;
+  }
+
+  @media (max-width: 420px) {
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 18px;
+    border-radius: 29px;
+  }
+`;
+
+export const Search = styled.p`
+  max-width: 432px;
+  min-width: 288px;
+  height: 48px;
+  background: #ffffff;
+  border: 1px solid #e4e6f0;
+  border-radius: 33px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  align-items: center;
+  color: #7e839a;
+
+  @media (max-width: 420px) {
+    height: 44px;
+  }
+`;
