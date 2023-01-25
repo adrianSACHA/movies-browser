750d964af15c2c245948bc1e194289dc35e58b2f
Konrad Kapica <konradkapica@gmail.com>
2023-01-25 12:19:13 +0100

    GlobalStyle and theme import changed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './core/App';
 import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './GlobalStyle';
-import { theme } from './theme'
+import { GlobalStyle } from './core/App/GlobalStyle';
+import { theme } from './core/App/theme'
 import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
