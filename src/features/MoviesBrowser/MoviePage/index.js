74fa9fb63423ee685962536dc6ebc14881e04824
Konrad Kapica <konradkapica@gmail.com>
2023-01-26 22:13:39 +0100

    MovieCast Component created

--- src/features/MoviesBrowser/MoviePage/index.js
+++ src/features/MoviesBrowser/MoviePage/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MovieTile from "./MovieTile";
 import Backdrop from "./Backdrop";
+import MovieCast from "./MovieCast";
 
 const MoviePage = () => (
     <>
         <Backdrop />
         <MovieTile />
+        <MovieCast />
     </>
 );
 
