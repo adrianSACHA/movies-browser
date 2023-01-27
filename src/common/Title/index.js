de938c54dbd192f03975764655309507a937c3d3
Konrad Kapica <konradkapica@gmail.com>
2023-01-28 00:33:03 +0100

    changes from pull

---
+++ src/common/Title/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+  color: ${({ theme }) => theme.colors.woodsmoke};
+`;
