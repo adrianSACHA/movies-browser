0e6a142c5c55fe060667d12f2cea144904ee9acf
Radek Bubienko <radek@bubienko.pl>
2023-01-27 20:42:55 +0100

    added react-router-dom

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { Route, Routes, Navigate, HashRouter } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
-import MoviesBrowser from "../../features/MoviesBrowser"
 import { theme } from "./theme";
 import Navigation from "../../common/Header/Navigation";
+import { toPopularMovies, toMoviePage } from "./routes";
+import PopularMovies from "../../features/MoviesBrowser/movies/MovieList/MovieListPage/PopularMovies";
+import MoviePage from "../../features/MoviesBrowser/movies/MoviePage";
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <Normalize />
       <GlobalStyle />
-      <Navigation />
-      <MoviesBrowser />
+      <HashRouter>
+        <Navigation />
+        <Routes>
+          <Route path={toPopularMovies} element={<PopularMovies />} />
+          <Route path={toMoviePage} element={<MoviePage />} />
+          <Route path={"*"} element={<Navigate replace to="/popularmovies" />} />
+
+        </Routes>
+      </HashRouter>
     </ThemeProvider>
   );
 }
