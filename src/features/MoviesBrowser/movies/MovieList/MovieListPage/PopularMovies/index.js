519cd1ea09026fd91a9015df7abe6188a2a54ad3
Radek Bubienko <radek@bubienko.pl>
2023-01-27 19:50:49 +0100

    created index.js in PopuMovies

---
+++ src/features/MoviesBrowser/movies/MovieList/MovieListPage/PopularMovies/index.js
@@ -0,0 +1,10 @@
+import { MainWrapper } from "../../../../../../common/MainWrapper";
+import { Title } from "../../../../../../common/Title";
+
+const PopularMovies = () => (
+    <MainWrapper>
+        <Title>PopularMovies</Title>
+    </MainWrapper>
+);
+
+export default PopularMovies;
