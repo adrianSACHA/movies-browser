60e8a328167b935d9f7e7a71a52c9cf718889d67
Konrad Kapica <konradkapica@gmail.com>
2023-01-28 15:19:22 +0100

    Movie Cast and Crew styled

--- src/features/MoviesBrowser/movies/MoviePage/MovieCast/styled.js
+++ src/features/MoviesBrowser/movies/MoviePage/MovieCast/styled.js
@@ -5,18 +5,47 @@ export const MovieCastWrapper = styled.div`
     margin: auto;
 `;
 
-export const HeaderMovieCast = styled.h2`
+export const HeaderMoviePeople = styled.h2`
     font-weight: 600;
     font-size: 36px;
     line-height: 1.2;
-    margin: 0 0 32px 0;
+    margin: 64px 0 32px 0;
     color: ${({ theme }) => theme.colors.black};
 `;
 
-export const ContainerMovieCast = styled.div`
+export const ContainerMoviePeople = styled.ul`
     max-width: 1368px;
     margin: auto;
     display: grid;
-
+    list-style: none;
 `;
 
+export const PersonTile = styled.article`
+    width: 208px;
+    height: 339px;
+    background: white;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const PersonImage = styled.img`    
+    width: 176px;
+    height: 231px;
+`;
+
+export const PersonName = styled.p`
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 1.3;
+    margin: 12px 0 8px 0;
+    color: #18181B;
+`;
+
+export const PersonInfo = styled.span`
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 1.5;
+    color: #7E839A;
+`;
