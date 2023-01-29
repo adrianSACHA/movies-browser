944fb73c3f76a0f68ae3b74a7ae1cd2280b04be1
Radek Bubienko <radek@bubienko.pl>
2023-01-29 12:12:26 +0100

    added Description in single movie to popular movies

--- src/features/MoviesBrowser/movies/MovieList/PopularMovies/index.js
+++ src/features/MoviesBrowser/movies/MovieList/PopularMovies/index.js
@@ -1,13 +1,84 @@
 import React from "react";
 import { MainWrapper } from "../../../../../common/MainWrapper";
 import { Title } from "../../../../../common/Title";
+import poster from "./images/poster.jpg";
+import { MoviesListWrapper, Image, MovieWrapper, LinkToMoviePage } from "./styled";
+import Description from "./Description";
+import { Rating } from "./Rating"; 
 
 const PopularMovies = () => (
+  <MainWrapper>
+    <Title>Popular Movies</Title>
     
-        <MainWrapper>
-        <Title>PopularMovies</Title>
-        </MainWrapper>
+      <MoviesListWrapper>
+        <LinkToMoviePage to={"/movie/:id"}>
+          <MovieWrapper>
+            <Image src={poster} alt="" />
+            <Description />
+            <Rating />
+          </MovieWrapper>
+        </LinkToMoviePage>
+
+        <LinkToMoviePage to={"/movie/:id"}>
+          <MovieWrapper>
+            <Image src={poster} alt="" />
+            <Description />
+            <Rating />
+          </MovieWrapper>
+        </LinkToMoviePage>
+
+        <LinkToMoviePage to={"/movie/:id"}>
+          <MovieWrapper>
+            <Image src={poster} alt="" />
+            <Description />
+            <Rating />
+          </MovieWrapper>
+        </LinkToMoviePage>
+
+        <LinkToMoviePage to={"/movie/:id"}>
+          <MovieWrapper>
+            <Image src={poster} alt="" />
+            <Description />
+            <Rating />
+          </MovieWrapper>
+        </LinkToMoviePage>
+
+        <LinkToMoviePage to={"/movie/:id"}>
+          <MovieWrapper>
+            <Image src={poster} alt="" />
+            <Description />
+            <Rating />
+          </MovieWrapper>
+        </LinkToMoviePage>
+
+        <LinkToMoviePage to={"/movie/:id"}>
+          <MovieWrapper>
+            <Image src={poster} alt="" />
+            <Description />
+            <Rating />
+          </MovieWrapper>
+        </LinkToMoviePage>
+
+        <LinkToMoviePage to={"/movie/:id"}>
+          <MovieWrapper>
+            <Image src={poster} alt="" />
+            <Description />
+            <Rating />
+          </MovieWrapper>
+        </LinkToMoviePage>
+
+        <LinkToMoviePage to={"/movie/:id"}>
+          <MovieWrapper>
+            <Image src={poster} alt="" />
+            <Description />
+            <Rating />
+          </MovieWrapper>
+        </LinkToMoviePage>
+        
+      </MoviesListWrapper>
+  </MainWrapper>
+      
     
 );
- 
+
 export default PopularMovies;
