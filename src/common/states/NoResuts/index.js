827c1c5fcbec45c8112773a1c30ea5fde9966993
Radek Bubienko <radek@bubienko.pl>
2023-01-29 22:57:23 +0100

    added component NoResults

---
+++ src/common/states/NoResuts/index.js
@@ -0,0 +1,12 @@
+import { PictureWrapper, Picture } from "./styled";
+import { MainWrapper } from "../../MainWrapper";
+
+export const NoResults = () => {
+    return (
+        <MainWrapper>
+            <PictureWrapper>
+                <Picture />
+            </PictureWrapper>
+        </MainWrapper>
+    );
+};
