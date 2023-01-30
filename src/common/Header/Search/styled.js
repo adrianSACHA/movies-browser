8b3f381c2353a9d2479e947a7b693980cef4e0d3
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-30 19:12:31 +0100

    added centered for Loop

--- src/common/Header/Search/styled.js
+++ src/common/Header/Search/styled.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
+import { ReactComponent as Search } from "./Search.svg";
 
 export const Wrapper = styled.div`
   width: 432px;
   height: 48px;
   display: flex;
+  align-items: center;
+  justify-content: center;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
     height: 44px;
@@ -13,16 +16,33 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const StyledInput = styled.input`
+export const MainSearch = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 19px;
   background: ${({ theme }) => theme.colors.white};
-  border: 1px solid ${({ theme }) => theme.colors.mystic};
   border-radius: 33px;
+  border: 1px solid ${({ theme }) => theme.colors.mystic};
+
+`;
+
+export const Loop = styled(Search)`
+align-items: center;
+justify-content: center;
+width: 24px;
+height: 24px;
+margin: 15px 0 15px 26px;
+
+`;
+
+export const StyledInput = styled.input`
   font-weight: 400;
   font-size: 16px;
   line-height: 150%;
-  align-items: center;
   color: ${({ theme }) => theme.colors.waterloo};
+  border: none;
+  width: 70%;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
+   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
   }
 `;
