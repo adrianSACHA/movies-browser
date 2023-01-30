f980c51ffe6234d21c77e1e926cfe09cc0018ff6
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-30 18:38:40 +0100

    switched Navigation to Header

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -3,25 +3,26 @@ import { Route, Routes, Navigate, HashRouter } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-import Navigation from "../../common/Header/Navigation";
+import  Header  from "../../common/Header/index";
 import { toPopularMovies, toMoviePage, toPeople } from "./routes";
 import PopularMovies from "../../features/movies/MovieList/PopularMovies";
 import MoviePage from "../../features/movies/MoviePage";
 import PeopleList from "../../features/people/PeopleList";
 
 function App() {
-  
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <HashRouter>
-        <Navigation />
+        <Header />
         <Routes>
           <Route path={toPopularMovies} element={<PopularMovies />} />
           <Route path={toMoviePage} element={<MoviePage />} />
           <Route path={toPeople} element={<PeopleList />} />
-          <Route path={"*"} element={<Navigate replace to="/popularmovies" />} />
-
+          <Route
+            path={"*"}
+            element={<Navigate replace to="/popularmovies" />}
+          />
         </Routes>
       </HashRouter>
     </ThemeProvider>
