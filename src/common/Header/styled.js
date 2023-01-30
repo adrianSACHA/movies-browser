014cfca0971353e80945d017e09f009d11232984
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-30 18:35:56 +0100

    created wrapper

---
+++ src/common/Header/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: ${({ theme }) => theme.colors.black};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
