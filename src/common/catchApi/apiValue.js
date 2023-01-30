2d15509313827437ae0acd6b40454e2f901820e3
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-30 11:54:41 +0100

    fix path api key

--- src/common/catchApi/apiValue.js
+++ src/common/catchApi/apiValue.js
@@ -1,4 +1,4 @@
-export const apiKey = "?api_key=9d9c05d0874949c20da7c07f1988fd47";
+export const apiKey = "9d9c05d0874949c20da7c07f1988fd47";
 export const apiBaseUrl = "https://api.themoviedb.org/3";
 export const language = "&language=en-US";
 export const imgBaseUrl = "https://image.tmdb.org/t/p";
