0227cb736fee39b0972ca526bc2eef0d3bb800fa
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-30 15:44:42 +0100

    Header component. moved from Navigation

---
+++ src/common/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Wrapper } from "./styled";
+import { Search } from "./Search";
+import { Navigation } from "./Navigation";
+
+const Navigation = () => (
+  <Wrapper>
+    <Navigation />
+    <Search />
+  </Wrapper>
+);
+export default Navigation;
