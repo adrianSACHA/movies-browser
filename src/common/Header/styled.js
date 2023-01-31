35dbc65f0b75161183657a444b0808643c487656
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-31 12:17:44 +0100

    added wrap and min height

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -3,7 +3,12 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   background: ${({ theme }) => theme.colors.black};
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-`;
+  min-height: 94px;
 
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    min-height: 142px;
+  }
+`;
