011b166baffae61ba4d596d347ef1e583c598a21
Konrad Kapica <konradkapica@gmail.com>
2023-02-02 14:32:59 +0100

    store.js created and saga connected

--- src/core/store.js
+++ src/core/store.js
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import movieDetailsReducer from "../features/movies/MoviePage/movieDetailsSlice"
+import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
-        movieDetails: movieDetailsReducer,
+        moviePage: movieDetailsReducer,
     },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
