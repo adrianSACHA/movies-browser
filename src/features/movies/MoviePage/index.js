b12d2ca2f5d36fa128f3e2c539bb1470b99c9797
Konrad Kapica <konradkapica@gmail.com>
2023-02-03 22:29:41 +0100

    api added to Backdrop and MovieTile

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -15,16 +15,17 @@ import { ErrorBox } from "../../../common/states/ErrorBox";
 import MovieTile from "./components/MovieTile";
 import Backdrop from "./components/Backdrop";
 import MovieCast from "./components/MovieCast";
+import MovieCrew from "./components/MovieCrew";
 
 
 
 const MoviePage = () => {
     const dispatch = useDispatch();
-    const {id} = useParams();
-    const movieId = useSelector(selectMovieId);
+    const { id } = useParams();
+    const movie = useSelector(selectMovieId);
     const cast = useSelector(selectMovieCast);
     const crew = useSelector(selectMovieCrew);
-    const stateLoading = useSelector(selectMovieStatus);
+    const stateOfStatus = useSelector(selectMovieStatus);
     const query = useQueryParameter("search");
 
     useEffect(() => {
@@ -33,17 +34,52 @@ const MoviePage = () => {
 
     return (
         <>
-        {stateLoading === "loading" 
-        ? (<Loading />) 
-        : stateLoading === "error"}
-        ? (<ErrorBox />)
-        : query === null ?
-            <Backdrop />
-            <MovieTile />
-            <MovieCast />
+            {stateOfStatus === "error" ? (
+                <ErrorBox />
+            ) : stateOfStatus !== "success" ? (
+                <Loading />
+            ) : query === null && (
+                <>
+                    {movie.map(({ original_title, vote_average, vote_count, backdrop_path, id }) => (
+                        <Backdrop
+                            key={id}
+                            title={original_title}
+                            rating={vote_average}
+                            votes={vote_count}
+                            backdrop_path={backdrop_path}
+                        />
+                    )
+                    )}
+                    {movie.map(({
+                        id, 
+                        poster_path, 
+                        title, 
+                        release_date, 
+                        production_countries, 
+                        vote_average,
+                        vote_count,
+                        overview,
+                        genres,
+                    }) => (
+                        <MovieTile 
+                        key={id}
+                        id={id}
+                        poster_path={poster_path}
+                        title={title}
+                        release_date={release_date}
+                        rating={vote_average}
+                        votes={vote_count}
+                        countries={production_countries}
+                        article={overview}
+                        genres={genres}
+                        />
+                    ))}
+                    <MovieCast />
+                    <MovieCrew />
+                </>
+            )}
         </>
-    )
-
+    );
 };
 
 export default MoviePage;
