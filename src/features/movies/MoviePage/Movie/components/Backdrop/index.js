0fdfe63ec70bd004caefde7ac729f25a73a5ae58
Konrad Kapica <konradkapica@gmail.com>
2023-02-06 15:29:32 +0100

    propsa added to Backdrop

--- src/features/movies/MoviePage/Movie/components/Backdrop/index.js
+++ src/features/movies/MoviePage/Movie/components/Backdrop/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { imgBaseUrl } from "../../../../../../common/catchApi/apiValue";
 import {
     BackdropContainer,
     Pleksa,
@@ -13,33 +14,25 @@ import {
     LongTitle,
 } from "./styled";
 
+const Backdrop = ({ movie }) => (
 
-const imageURL = "https://image.tmdb.org/t/p";
-
-
-const Backdrop = (title, rating, votes, backdrop_path) => {
-
-    return (
-        <>
-            {backdrop_path && (
-                <BackdropWrapper>
-                    <BackdropContainer>
-                        <Pleksa />
-                        <BackdropImage src={`${imageURL}/w1280${backdrop_path}`} alt={`poster of ${title}`} />
-                        <BackdropInfoContainer>
-                            <LongTitle>{title}</LongTitle>
-                            <Rating>
-                                <StarIcon />
-                                <RateBig>{rating}</RateBig>
-                                <RateSmall>/ 10</RateSmall>
-                                <Votes>{votes} votes</Votes>
-                            </Rating>
-                        </BackdropInfoContainer>
-                    </BackdropContainer>
-                </BackdropWrapper>
-            )}
-        </>
-    );
-};
+        <BackdropWrapper>
+            {movie.backdrop_path && movie.map((backdrop_path, title, vote_average, vote_count) => (
+                <BackdropContainer>
+                    <Pleksa />
+                    <BackdropImage src={`${imgBaseUrl}/w1280${backdrop_path}`} alt={`poster of ${title}`} />
+                    <BackdropInfoContainer>
+                        <LongTitle>{title}</LongTitle>
+                        <Rating>
+                            <StarIcon />
+                            <RateBig>{vote_average}</RateBig>
+                            <RateSmall>/ 10</RateSmall>
+                            <Votes>{vote_count} votes</Votes>
+                        </Rating>
+                    </BackdropInfoContainer>
+                </BackdropContainer>
+            ))}
+        </BackdropWrapper>
+);
 
 export default Backdrop;
