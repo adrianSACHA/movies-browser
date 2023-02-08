41b17775b9c0573888817c5025559ae6354845ca
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-08 12:34:59 +0100

    added Person to Routes

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -4,24 +4,28 @@ import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { Navigation } from "../../common/Navigation";
-import { toPopularMovies, toMoviePage, toPeople } from "./routes";
+import { toPopularMovies, toMoviePage, toPeople, toPerson } from "./routes";
 import PopularMovies from "../../features/movies/MovieList/PopularMovies";
 import MoviePage from "../../features/movies/MoviePage";
 import PeopleList from "../../features/people/PeopleList";
+import Person from "../../features/people/Person";
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <HashRouter>
+      <HashRouter >
         <Navigation />
         <Routes>
           <Route path={toPopularMovies} element={<PopularMovies />} />
           <Route path={toMoviePage} element={<MoviePage />} />
           <Route path={toPeople} element={<PeopleList />} />
+          <Route path={toPerson} element={<Person />} />
+
           <Route
-            path={"*"}
-            element={<Navigate replace to="/popularMovies" />}
+            path="/"
+            element={<Navigate replace to={toPopularMovies} />}
           />
         </Routes>
       </HashRouter>
