d5ae0d2abf709def8b0a25dd35d11c109b475fce
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-08 10:27:04 +0100

    changed path to Navigation

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -3,7 +3,7 @@ import { Route, Routes, Navigate, HashRouter } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-import Header from "../../common/Header/index";
+import { Navigation } from "../../common/Navigation";
 import { toPopularMovies, toMoviePage, toPeople } from "./routes";
 import PopularMovies from "../../features/movies/MovieList/PopularMovies";
 import MoviePage from "../../features/movies/MoviePage";
@@ -14,14 +14,14 @@ function App() {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <HashRouter>
-        <Header />
+        <Navigation />
         <Routes>
           <Route path={toPopularMovies} element={<PopularMovies />} />
           <Route path={toMoviePage} element={<MoviePage />} />
           <Route path={toPeople} element={<PeopleList />} />
           <Route
             path={"*"}
-            element={<Navigate replace to="/popularmovies" />}
+            element={<Navigate replace to="/popularMovies" />}
           />
         </Routes>
       </HashRouter>
