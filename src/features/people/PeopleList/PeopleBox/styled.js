83eab0276537eb078e2bba45967a2db6177a5cc6
Radek Bubienko <radek@bubienko.pl>
2023-02-10 21:47:15 +0100

    made styles on popular people

--- src/features/people/PeopleList/PeopleBox/styled.js
+++ src/features/people/PeopleList/PeopleBox/styled.js
@@ -4,11 +4,11 @@ import noImage from "./images/profile.svg";
 
 export const PeopleListWrapper = styled.section`
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   gap: 24px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(4, 1fr);
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
@@ -16,7 +16,7 @@ export const PeopleListWrapper = styled.section`
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
-    grid-template-columns: 1fr;
+    grid-template-columns: repeat(2, 1fr);
     margin: 12px auto 0;
   }
 `;
@@ -42,16 +42,19 @@ export const PersonWrapper = styled.div`
   background: #ffffff;
   display: grid;
   grid-template-rows: auto 1fr;
-  
-  box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+  transition: 0.5s;
+  box-shadow: 0px 4px 12px rgba(186, 199, 213, 1);
   border-radius: 5px;
+  padding: 16px;
 
   &:hover {
+    transform: scale(1.03);
     box-shadow: ${({ theme }) => theme.hoverShadow};
   }
 
   &:active {
     box-shadow: ${({ theme }) => theme.activeShadow};
+    transform: scale(1.05);
   }
 
 `;
@@ -78,7 +81,6 @@ export const Image = styled.img`
   width: 100%;
   height: auto;
   border-radius: 5px;
-  padding: 16px;
   aspect-ratio: 2/3;
 `;
 
@@ -89,13 +91,18 @@ export const Description = styled.div`
 `;
 
 export const Name = styled.p`
-  margin: 0;
+  margin: 16px 0 0 0;
   font-weight: 400;
-  font-size: 18px;
-  line-height: 1.5;
-  color: ${({ theme }) => theme.colors.waterloo};
+  font-size: 22px;
+  line-height: 1.3;
+  color: ${({ theme }) => theme.colors.woodsmoke};
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMed}) {
-    font-size: 13px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMed}px) {
+    font-size: 18px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
+    margin: 12px 0 0 0;
+    font-size: 16px;
   }
 `;
