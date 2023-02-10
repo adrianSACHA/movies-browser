16c2025285042e9ff06f4a90df3522b29fd7d794
Radek Bubienko <radek@bubienko.pl>
2023-02-10 22:50:21 +0100

    search component working

--- src/common/states/NoResults/index.js
+++ src/common/states/NoResults/index.js
@@ -2,10 +2,15 @@
 import React from "react";
 import { PictureWrapper, Picture } from "./styled";
 import { MainWrapper } from "../../MainWrapper";
+import { Title } from "../../Title/index";
+import { searchQueryParamName, useQueryParameter } from "../../Query/queryParameters";
 
 export const NoResults = () => {
+const query = useQueryParameter(searchQueryParamName);
+
     return (
         <MainWrapper>
+            <Title title={`Sorry, there are no result for "${query}"`}/>
             <PictureWrapper>
                 <Picture />
             </PictureWrapper>
