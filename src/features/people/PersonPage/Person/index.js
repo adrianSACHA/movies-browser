4c8789c4384a3b89d4dfce3d751cb9ba8970a8de
Konrad Kapica <konradkapica@gmail.com>
2023-02-11 13:41:24 +0100

    movieBox from movieList used

--- src/features/people/PersonPage/Person/index.js
+++ src/features/people/PersonPage/Person/index.js
@@ -3,18 +3,13 @@ import { useSelector } from "react-redux";
 import {
     selectPersonDetails,
     selectPersonCredits,
-} from "../movieDetailsSlice";
-import MovieTile from "./components/MovieTile";
+} from "../personDetailsSlice";
+import PersonTile from "./PersonTile";
 import {
     HeaderMoviePeople,
-    ContainerMoviePeople,
-    PersonLink,
-    PersonName,
-    PersonInfo,
-    PersonImage,
 } from "../../../../common/Credits/styled";
 import { MainWrapper } from "../../../../common/MainWrapper";
-import picture from "../../../../common/Credits/Picture.svg";
+import MovieBox from "../../../movies/MovieList/MovieBox";
 
 const PersonPageDetails = () => {
     const person = useSelector(selectPersonDetails);
@@ -25,58 +20,28 @@ const PersonPageDetails = () => {
     return (
         <>
             {person.profile_path && (
-                <MovieTile
+                <PersonTile
                     poster_path={person.profile_path}
                     title={person.name}
                     birthday={person.birthday}
                     place={person.place_of_birth}
-                    biography={person.biography}
+                    description={person.biography}
                 />
             )}
             {cast.length > 0 && (
                 <MainWrapper>
-                    <HeaderMoviePeople>Cast</HeaderMoviePeople>
-                    <ContainerMoviePeople>
-                        {cast.map((actor) =>
-                            <PersonLink key={actor.id}
-                            >
-                                {actor.profile_path ? (
-                                    <PersonImage src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt="" />
-                                ): <PersonImage src={picture} alt="" />}
-                                {actor.name && (
-                                    <PersonName>{actor.name}</PersonName>
-                                )}
-                                {actor.character && (
-                                    <PersonInfo>{actor.character}</PersonInfo>
-                                )}
-                            </PersonLink>
-                        )}
-                    </ContainerMoviePeople>
+                    <HeaderMoviePeople>Movies - cast ({cast.length})</HeaderMoviePeople>
+                        <MovieBox movies={cast} />
                 </MainWrapper>
             )}
             {crew.length > 0 && (
                 <MainWrapper>
-                    <HeaderMoviePeople>Crew - </HeaderMoviePeople>
-                    <ContainerMoviePeople>
-                        {crew.map((member) =>
-                            <PersonLink key={member.id}
-                            >
-                                {member.profile_path ? (
-                                    <PersonImage src={`https://image.tmdb.org/t/p/w500/${member.profile_path}`} alt="" />
-                                ): <PersonImage src={picture} alt="" />}
-                                {member.name && (
-                                    <PersonName>{member.name}</PersonName>
-                                )}
-                                {member.department && (
-                                    <PersonInfo>{member.department}</PersonInfo>
-                                )}
-                            </PersonLink>
-                        )}
-                    </ContainerMoviePeople>
+                    <HeaderMoviePeople>Movies - crew ({crew.length})</HeaderMoviePeople>
+                            <MovieBox movies={crew} />
                 </MainWrapper>
             )}
         </>
     );
 };
 
-export default MoviePageDetails;
+export default PersonPageDetails;
