8161df02bc4a3e617210aa0496db140a2c2472b5
Konrad Kapica <konradkapica@gmail.com>
2023-02-11 09:06:23 +0100

    added personDetailsSaga to saga.js

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,7 +1,12 @@
 import { all } from "redux-saga/effects";
 import { movieListSaga } from "../features/movies/MovieList/movieListSaga";
 import { movieDetailsSaga } from "../features/movies/MoviePage/movieDetailsSaga";
+import { personDetailsSaga } from "../features/people/PersonPage/personDetailsSaga";
 
 export default function* saga() {
-    yield all([movieListSaga(), movieDetailsSaga()]);
+    yield all([
+        movieListSaga(), 
+        movieDetailsSaga(),
+        personDetailsSaga(),
+    ]);
 }
